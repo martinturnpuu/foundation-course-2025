768964dc87fd8d4ac753c11f319bab643f45a98d
martinturnpuu <martun.w@gmail.com>
2025-10-08 19:50:12 +0300

    8 10
    lab9

---
+++ labs/lab 7/script.js
@@ -0,0 +1,30 @@
+
+
+
+let firstName ="asaqwtqe";
+
+let newSentence = "Hello I am " + firstName;"I am called " + firstName; "... " + firstName; "saffsfdsfsdf" + firstName;
+ 
+console.log(newSentence);
+
+//console.log(Hello I am ${firstName},I am called ${firstName}, ... ${firstName}, saffsfdsfsdf ${firstName},");
+alert("hi")
+let num1;
+const num2 = 5;
+
+num1 = 10;
+
+let result = num1;
+console.log(result);
+
+let mixedResult = firstName + num1;
+console.log(mixedResult);
+
+let a = 1;
+let b = 3;
+
+console.log(a+b); //Addition:13
+console.log(a-b); //Substraction:7
+console.log(a*b); //Multiplaction:30
+console.log(a/b); //Division:3.333...
+console.log(a%b); //Reminder of division
---
+++ labs/lab 8/script.js
@@ -0,0 +1,39 @@
+let grades = [50,89,75,84,99];
+let sum = 0;
+
+for(let i = 0; i<5; i++)
+{
+    sum += grades [i];
+
+}
+
+console.log(sum);
+let result = sum / 5
+console.log(result);
+
+
+
+
+
+
+
+
+
+/*
+const grade = promt("Enter the grade");
+console.log(grade);
+
+if (grade > 90) 
+    {
+  console.log("Your grade is A");
+} else if (grade > 80) 
+    {
+  console.log("Your grade is B");
+} else if (grade > 70) 
+    {
+  console.log("Your grade is C");
+} else if (grade > 50) {
+  console.log("Your grade is D");
+} 
+*/
+
