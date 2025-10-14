a68573e4332313279f014489669e49b486e3937f
martinturnpuu <martun.w@gmail.com>
2025-10-14 16:04:47 +0300

    lab10 HW
    10 14

--- labs/Lab10/site2/color.js
+++ labs/Lab10/site2/color.js
@@ -1,17 +1,24 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["#dfe22fff", "#253b9dff", "#259d81ff", "#f15025",
+"rgba(100,22,33,0.8)","rgba(0, 0, 0, 0.8)","rgba(218, 199, 35, 0.8)"
+
+];
+const rgba = ["rgba(100,22,33,0.8)","rgba(0, 0, 0, 0.8)","rgba(218, 199, 35, 0.8)"];
+const hex = ["#ffc1b1","#c44121ff","#57c421ff","#2621c4ff"];
+
+
 
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
+
 const colorText = document.querySelector('#colorCode');
 const colorPanel = document.querySelector('#colorPanel');
-
 
 
 function randomColor()
 {
     console.log('First button got clicked!!!');
-    let colorIndex = Math.floor(Math.random()*colors.lenght);
+    let colorIndex = Math.floor(Math.random()*colors.length);
     console.log(colorIndex);
     colorPanel.style.backgroundColor = colors[colorIndex];
     colorText.innerText =  colors[colorIndex];
@@ -19,3 +26,35 @@ function randomColor()
 }
 
 btn1.addEventListener('click',randomColor);
+//use this format to change the color rgba
+//rgba(r,g,b) 0 
+
+
+function randomRgba()
+{
+    console.log('Second button got clicked!!!');
+    let colorIndex = Math.floor(Math.random()*rgba.length);
+    console.log(colorIndex);
+    colorPanel.style.backgroundColor = rgba[colorIndex];
+    colorText.innerText =  rgba[colorIndex];
+
+}
+
+btn2.addEventListener('click',randomRgba);
+//use this format to change the color rgba
+//rgba(r,g,b) 0 
+
+
+function randomHex()
+{
+    console.log('Third button got clicked!!!');
+    let colorIndex = Math.floor(Math.random()*hex.length);
+    console.log(colorIndex);
+    colorPanel.style.backgroundColor = hex[colorIndex];
+    colorText.innerText =  hex[colorIndex];
+
+}
+
+btn3.addEventListener('click',randomHex);
+//use this format to change the color rgba
+//rgba(r,g,b) 0 
